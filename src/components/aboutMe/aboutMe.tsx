207ad25071e64ba868d6dae17aa865147062d74b
Melroy Glan Pinto <melroypinto16@gmail.com>
2022-02-06 13:42:59 +0530

    hobbie setup

--- src/components/aboutMe/aboutMe.tsx
+++ src/components/aboutMe/aboutMe.tsx
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import styles from './aboutMe.module.scss';
 import cx from 'classnames';
 import Image from 'next/image';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import { Images } from '~/src/utils/icon.constant';
 import { socialIcons } from '~/src/common/socialIcons';
-import { hobbies, siteConst } from '~/src/utils/constants';
+import { hobbies, siteConst, skills } from '~/src/utils/constants';
 
 const AboutMe = () => {
     return (
         <div className={cx(styles.paddingTop, 'container')}>
             <div className={styles.profileContainer}>
+                <h1>About Me</h1>
                 <Row>
                     <Col lg={{ span: 3 }} className={styles.alignCenter}>
                         <Image src={Images.profilePic} alt="Logo" className={styles.profilePic} width="150" height="150" />
@@ -20,18 +21,41 @@ const AboutMe = () => {
                     </Col>
                     <Col lg={{ span: 9 }}>{siteConst.aboutMe}</Col>
                 </Row>
-            </div>
-            <div className={styles.marginTop}>
-                <Row>
-                    {hobbies.map((hobby) => (
-                        <Col lg={{ span: 3 }} className={cx(styles.alignCenter, styles.paddingTopBottom)} key={hobby.key}>
-                            <div className={styles.hobbiesContainer}>
-                                <Image src={Images.profilePic} alt="Logo" className={styles.profilePic} width="100" height="100" />
-                                <p className={styles.userName}>Melroy Glan Pinto</p>
-                            </div>
-                        </Col>
-                    ))}
-                </Row>
+                <div className={styles.marginTop}>
+                    <h2>My Skills</h2>
+                    <Row>
+                        {skills.map((skill) => (
+                            <Col lg={{ span: 4 }} className={cx(styles.alignCenter, styles.paddingTopBottom)} key={skill.key}>
+                                <div className={styles.hobbiesContainer}>
+                                    <Image src={skill.img} alt="Logo" height={skill.height} width={skill.width} />
+                                    <p className={styles.userName}>{skill.title}</p>
+                                </div>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+                <div className={styles.marginTop}>
+                    <h3>Below are the things give me Inner Peace</h3>
+                    <Row>
+                        {hobbies.map((hobby) => (
+                            <Col lg={{ span: 4 }} className={cx(styles.alignCenter, styles.paddingTopBottom)} key={hobby.key}>
+                                {/* <div className={styles.hobbiesContainer}>
+                                    <Image src={Images.profilePic} alt="Logo" width={100} height="100" />
+                                    <p className={styles.userName}>{hobby.title}</p>
+                                </div> */}
+                                <Card>
+                                    <Image src={Images.profilePic} alt="Logo" width={100} height="250" className={styles.hobbies}/>
+                                    <Card.Body>
+                                        <Card.Title>{hobby.title}</Card.Title>
+                                        <Card.Text>
+                                            {hobby.desc}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
             </div>
         </div>
     );
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -5,21 +5,63 @@ export const siteConst = {
 
 export const hobbies = [
     {
-        key: 1
+        key: 1,
+        title: 'Playing CS:GO',
+        desc: ''
     },
     {
-        key: 2
+        key: 2,
+        title: 'Watching CS:GO',
+        desc: ''
     },
     {
-        key: 3
+        key: 3,
+        title: 'Dogs',
+        desc: ''
+    },
+]
+
+export const skills = [
+    {
+        key: 1,
+        img: require('../../public/images/angularLogo.png'),
+        width: 95,
+        height: 100,
+        title: 'Angular'
     },
     {
-        key: 4
+        key: 2,
+        img: require('../../public/images/reactLogo.png'),
+        width: 100,
+        height: 100,
+        title: 'React'
     },
     {
-        key: 5
+        key: 3,
+        img: require('../../public/images/nodeJsLogo.png'),
+        width: 200,
+        height: 100,
+        title: 'Node Js'
     },
     {
-        key: 6
+        key: 4,
+        img: require('../../public/images/wordPressLogo.png'),
+        width: 100,
+        height: 100,
+        title: 'WordPress'
+    },
+    {
+        key: 5,
+        img: require('../../public/images/shopifyLogo.png'),
+        width: 85,
+        height: 100,
+        title: 'Shopify'
+    },
+    {
+        key: 6,
+        img: require('../../public/images/jsTsLogo.png'),
+        width: 200,
+        height: 100,
+        title: 'TypeScript & JavaScript'
     },
 ]
